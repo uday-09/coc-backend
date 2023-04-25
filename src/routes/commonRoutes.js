3b3d6e30abc90f08130cc454a1e0407fd1472230
uday-09 <udayruby09@gmail.com>
2023-04-26 00:12:39 +0530

    send mails added

--- src/routes/commonRoutes.js
+++ src/routes/commonRoutes.js
@@ -1,39 +1,69 @@
 const express = require("express");
-const authUser = require("../middleware/authUser");
-const Post = require("../models/post");
 const router = express.Router();
-const User = require("../models/user");
 const multer = require("multer");
+const sgMail = require("@sendgrid/mail");
+const hbs = require("handlebars");
+const fs = require("fs");
+const path = require("path");
 
-router.get("/common/rejected", async (req, res) => {
-  return res.send({ message: "success", success: true });
-});
+sgMail.setApiKey(process.env.SG_MAIL_KEY);
 
-const upload = multer({
-  dest: "posts",
-  limits: {
-    fileSize: 5000000,
-  },
-  fileFilter(req, file, cb) {
-    if (
-      !file.originalname.endsWith(".mp4") &&
-      !file.originalname.endsWith(".mkv")
-    ) {
-      return cb(new Error("Only MP4 files are allowed"));
-    }
-    cb(undefined, true);
-  },
-});
+router.post("/send/post-status", async (req, res) => {
+  const { reciever, reasons, imageUri, postTitle, username } = req.body;
 
-router.post(
-  "/post/video",
-  upload.single("upload"),
-  async (req, res) => {
-    res.send();
-  },
-  (err, req, res, next) => {
-    res.status(500).send({ error: err.message });
+  console.log(req.body);
+
+  if (!reciever || !reasons || !imageUri || !postTitle) {
+    return res.status(400).send({
+      message: "Reciever mail, reasons, image URL and post title are required",
+      success: false,
+    });
   }
-);
+
+  var source = `
+  <strong>Hey {{username}}!</strong>
+  <p>We thank you for supporting us in bringing better society by sharing the crime you came across. But we are sorry to inform you that the post you have uploaded recently got rejected!
+  </p>
+  <strong>We have attached image related to that post:</strong>
+  <img src={{imageUri}}/>
+  <p>Your post has been rejected due to following reasons: </p>
+  <ul>
+  {{#each reasons}}
+  <li>{{this.reason}}</li>
+  {{/each}}
+  </ul>
+  <p><b><i>Please try to improve the post next time!</i></b></p>
+
+  <p>Regards,</p>
+
+  <p>Admin, Cop-on-Cloud </p>
+  <br />
+  <img src="https://cop-on-cloud.s3.us-east-1.amazonaws.com/image-1681067033573" width="50px" height="50px" />
+  `;
+  var template = hbs.compile(source);
+
+  var data = {
+    username,
+    imageUri,
+    reasons,
+  };
+  var result = template(data);
+
+  const msg = {
+    to: reciever, // Change to your recipient
+    from: "coponcloud23@gmail.com", // Change to your verified sender
+    subject: `Rejecting post: ${postTitle}`,
+    html: result,
+  };
+
+  try {
+    sgMail.send(msg);
+    // return res.send(result);
+    // return res.send({ message: "Mail sent succesfully!", success: true });
+    res.send({ success: true, message: "Email send succesfully!" });
+  } catch (err) {
+    res.send({ success: false, message: err.message });
+  }
+});
 
 module.exports = router;
